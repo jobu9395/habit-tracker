b81b401daaa3a739050c95cca6d5d58f7b207b13
jobu9395 <jburdett1993@gmail.com>
2023-01-29 15:54:04 -0600

    working form submission

---
+++ README.md
@@ -0,0 +1,1 @@
+run ```python app.py``` to run app in debug mode locally
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+habits = ["Test habit", "Test habit 2", "Test Habit 3"]
+
+@app.route("/")
+def index():
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_habit():
+    if request.method == "POST":
+        habits.append(request.form.get("habit"))
+    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+
+if __name__ == '__main__':
+    app.run(debug=True)
